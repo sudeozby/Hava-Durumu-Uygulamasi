f3bb17ed0c9bb7471b1cf877b5cac9d99dc554fd
Sudeozbay <24181616031@gazi.edu.tr>
2026-05-12 18:43:08 +0300

    clear fonk yüzünden yaşanılan tema kaybı düzeltildi

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 $(document).ready(function() {
-    localStorage.clear(); // Temiz bir başlangıç
+    
 
     // =================================================================
     // GENEL DEĞİŞKENLER
